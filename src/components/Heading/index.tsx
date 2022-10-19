07b007d94be39bd296754897baa013a0cc2a197a
rcandrade <rcandrade@rdm504017.local>
2022-10-19 10:05:08 -0300

    refactor: finish design system

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -5,18 +5,21 @@ import { Slot } from '@radix-ui/react-slot';
 export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
+    className?: string;
     asChild?: boolean;
 }
 
-export function Heading({size = 'md', children, asChild}: HeadingProps){
+export function Heading({size = 'md', children, asChild, className}: HeadingProps){
     const Comp = asChild ? Slot : 'h2';
 
     return (
-        <Comp className={clsx("text-gray-400 font-bold", {
+        <Comp className={clsx("text-gray-100 font-bold", {
             'text-lg' : size === 'sm',
             'text-xl' : size === 'md',
             'text-2xl' : size === 'lg',
-        })}>
+        },
+        className,
+        )}>
             {children}
         </Comp>
     );
